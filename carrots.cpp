7bf79723b02af3c1e68e26550afe5f9863465fa3
Vaibhav Dachavaram <79995781+CoderVaibhavD@users.noreply.github.com>
2021-05-10 18:29:26 +0100

    Add files via upload
    https://open.kattis.com/problems/carrots
    
    Problem: Solving For Carrots

---
+++ carrots.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int contestants, problems;
+    string other;
+    cin >> contestants >> problems;
+
+    for (int i = 0; i<contestants; i++)
+    {
+        cin >> other;
+    }
+    cout << problems;
+
+    return 0;
+
+}
