fd17076bcbeebad1d4db4e1e9330e63688995b38
Vaibhav Dachavaram <79995781+CoderVaibhavD@users.noreply.github.com>
2021-05-10 18:28:15 +0100

    Add files via upload
    https://open.kattis.com/problems/r2

---
+++ r2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    long int rone, s;
+    cin >> rone >> s;
+
+    long int rtwo = (s*2)-rone;
+    cout << rtwo;
+
+    return 0;
+
+}
