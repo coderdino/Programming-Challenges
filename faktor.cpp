b568d8eb43c433f488261e90dce12333fbd1d4cf
Vaibhav Dachavaram <79995781+XxCoderVaibhavxX@users.noreply.github.com>
2021-05-18 17:35:03 +0100

    Add files via upload

---
+++ faktor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    int i;
+    int c;
+    int d;
+    
+    cin >> a >> i;
+    
+    c = a*i;
+    a--;
+    d = c-a;
+    cout << d;
+
+    return 0;
+}
