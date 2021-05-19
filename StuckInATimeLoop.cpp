79fcfba09944c21138fef264922b86e845e35918
Vaibhav Dachavaram <79995781+XxCoderVaibhavxX@users.noreply.github.com>
2021-05-19 18:04:26 +0100

    Create StuckInATimeLoop.cpp

---
+++ StuckInATimeLoop.cpp
@@ -0,0 +1,15 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int num;
+    cin >> num;
+    
+    for(int i = 1; i<=num; i++)
+    {
+        cout << i << " Abracadabra" << endl;
+    }
+
+    return 0;
+}
