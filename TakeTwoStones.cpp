8f612dbee79965674e8f418ee6b4029ed75f90b1
Vaibhav Dachavaram <79995781+XxCoderVaibhavxX@users.noreply.github.com>
2021-05-19 18:10:45 +0100

    Create TakeTwoStones.cpp

---
+++ TakeTwoStones.cpp
@@ -0,0 +1,19 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int num;
+    cin >> num;
+    
+    if(num % 2 == 0)
+    {
+        cout << "Bob";
+    }
+    else
+    {
+        cout << "Alice";
+    }
+
+    return 0;
+}
